a8dff64355873d06bc44d76fb8142e9b09170864
Nikhil <nikhilkhavdu441@.com>
2025-03-27 18:33:31 +0530

    create tabs

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex items-center justify-center flex-1 h-screen ">
+    </View>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,15 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import './global.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar style="dark" />  
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
 }
